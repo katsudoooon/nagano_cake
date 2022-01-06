fee06b7ff9a58272d663897533abf036b34c6c5e
katsudoooon <sub62.mail58address@gmail.com>
2022-01-06 15:06:03 +0000

    [Update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admin, controllers:{
+    sessions: 'admin/sessions',
+    registrations: 'admin/registrations'
+  }
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
 
 end
