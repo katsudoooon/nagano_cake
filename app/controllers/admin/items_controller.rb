fc4a9110549e4d02c5aee5af59a919a1ebd66214
katsudoooon <sub62.mail58address@gmail.com>
2022-02-19 14:25:01 +0000

    [Update]Itemデータベース登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @item = Item.new
+
   end
 
   def new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_142035) do
+ActiveRecord::Schema.define(version: 2022_02_19_142320) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -62,6 +62,12 @@ ActiveRecord::Schema.define(version: 2022_02_17_142035) do
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre_id"
+    t.string "name"
+    t.string "image_id"
+    t.text "introduction"
+    t.integer "price"
+    t.boolean "is_active"
   end
 
   create_table "order_details", force: :cascade do |t|
