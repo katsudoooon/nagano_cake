d7725ec3b948dbb633e2461f952f597eed00f99c
katsudoooon <sub62.mail58address@gmail.com>
2022-02-26 13:36:11 +0000

    [Update]nagano_cake itemほぼ完成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,6 +11,7 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+
     @item.save
     redirect_to admin_items_path
 
@@ -21,13 +22,17 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
   end
 
   private
   def item_params
-    params.require(:item).permit(:genre_id, :name, :image_id, :introduction, :price, :is_active)
+    params.require(:item).permit(:genre_id, :name, :image, :introduction, :price, :is_active)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd1d9c8e75190871f93cb135a9a609fe76fa667b8fcb54ace6c86c700bec1f3b5d1c41f133d3b4d923808946010a696bdde56472ebeb42722135dea0cc7a99c41'
