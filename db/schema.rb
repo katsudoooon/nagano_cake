10c445b67e07b66d6c4158c633835aa165669c26
katsudoooon <sub62.mail58address@gmail.com>
2022-02-28 14:41:23 +0000

    [Update]サインアップ完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_142320) do
+ActiveRecord::Schema.define(version: 2022_02_28_142710) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_02_19_142320) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
