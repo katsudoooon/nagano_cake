7feb512e086af6e917be5db1a55f415671e6aad7
katsudoooon <sub62.mail58address@gmail.com>
2022-03-01 14:50:35 +0000

    [Update]Customer途中まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  namespace :admin do
     get 'items/index'
     get 'items/new'
     get 'items/create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_142710) do
+ActiveRecord::Schema.define(version: 2022_03_01_144913) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_02_28_142710) do
     t.string "telephone_namber"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "telephone_number"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
